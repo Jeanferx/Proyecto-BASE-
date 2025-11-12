1f8ed9f85cc42e6b82c35b54f40455b664602e71
jean castro <castrojean848@gmail.com>
2025-11-11 19:34:45 -0500

    chore(menu)
    cambios en el typeScript de menu

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -3,7 +3,6 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.html',
-
   styleUrls: ['./menu.css'],
 })
 export class Menu {
