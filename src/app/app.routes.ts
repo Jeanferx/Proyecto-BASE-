083dec6af161eff7a11c283c02407aeb57340139
jean castro <castrojean848@gmail.com>
2025-11-12 13:48:41 -0500

    chorse(TS)
    cambios del router y app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Principal } from './principal/principal';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Principal },
+    { path: 'login', component: Login },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,18 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Menu } from './menu/menu';
 import { Copyright } from './copyright/copyright';
-import { Principal } from './principal/principal';
 import { Login } from './login/login';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Menu, Principal, Copyright, Login],
+  imports: [CommonModule, RouterOutlet, Menu, Copyright, Login],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   showLogin = false;
-
   onLoginClicked() {
     this.showLogin = true;
   }
