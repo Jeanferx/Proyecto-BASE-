3acf5801d83cc3a38e3f60b79d45c2c4e007e8c9
jean castro <castrojean848@gmail.com>
2025-11-11 19:06:32 -0500

    feat(lista-user)
    Creación de la carpeta lista user

---
+++ src/app/lista-user/lista-user.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-user',
+  imports: [],
+  templateUrl: './lista-user.html',
+  styleUrl: './lista-user.css',
+})
+export class ListaUser {
+
+}
