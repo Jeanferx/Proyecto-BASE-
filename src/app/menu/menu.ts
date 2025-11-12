24f670cf850dc77a1710790cbd048a22ad6bb9df
jean castro <castrojean848@gmail.com>
2025-11-12 11:18:07 -0500

    chore(menu)
    Mantenimiento del menu con angular material

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,8 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.html',
+  imports: [MatButtonModule, MatMenuModule],
   styleUrls: ['./menu.css'],
 })
 export class Menu {
