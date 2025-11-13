60127bd929b43145a4ecd62632b348ca880c5b88
jean castro <castrojean848@gmail.com>
2025-11-13 12:13:38 -0500

    refactor(app)
    Instalación del sweetalert2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RegistrarUser } from './registrar-user/registrar-user';
 import { Principal } from './principal/principal';
 import { Login } from './login/login';
+import { ListaUser } from './lista-user/lista-user'
 
 export const routes: Routes = [
     { path: '', component: Principal },
     { path: 'login', component: Login },
-    { path: 'registrar', component: RegistrarUser }
+    { path: 'registrar', component: RegistrarUser },
+    { path: 'lista', component: ListaUser }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,24 +5,32 @@ import { Menu } from './menu/menu';
 import { Copyright } from './copyright/copyright';
 import { Login } from './login/login';
 import { RegistrarUser } from './registrar-user/registrar-user';
+import { ListaUser } from './lista-user/lista-user'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Menu, Copyright, Login, RegistrarUser],
+  imports: [CommonModule, RouterOutlet, Menu, Copyright, Login, RegistrarUser, ListaUser],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   showLogin = false;
   showRegister = false;
-
+  showLista = false;
   onLoginClicked() {
     this.showLogin = true;
     this.showRegister = false;
+    this.showLista = false;
   }
 
   onRegisterClicked() {
     this.showRegister = true;
     this.showLogin = false;
+    this.showLista = false;
+  }
+  onListaClicked() {
+    this.showRegister = false;
+    this.showLogin = false;
+    this.showLista = true;
   }
 }
