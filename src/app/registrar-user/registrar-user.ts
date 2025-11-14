55879ff709938c1de69165810e1e0527a14cdad8
jean castro <castrojean848@gmail.com>
2025-11-14 12:55:26 -0500

    refactory(register)
    Cambios para la conexión a la api

--- src/app/registrar-user/registrar-user.ts
+++ src/app/registrar-user/registrar-user.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-user',
-  imports: [],
   standalone: true,
+  imports: [FormsModule, HttpClientModule],  // ⬅️ AGREGA ESTO
   templateUrl: './registrar-user.html',
   styleUrl: './registrar-user.css',
 })
 export class RegistrarUser {
+  constructor(private http: HttpClient) { }
 
+  Usuario = {
+    nombre: '', apellido: '', edad: 0, genero: '', correo: '', password: '', rol: ''
+  };
+
+  guardarEmpleado() {
+    this.http.post('https://localhost:7027/WeatherForecast', this.Usuario)
+      .subscribe(res => console.log('Empleado guardado:', res));
+  }
 }
